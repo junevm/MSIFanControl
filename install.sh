acf64cf7e0fc3766c2305d32c5a28c0ca926f5ef
aV <9752193-a0v0@users.noreply.gitlab.com>
2025-05-05 22:02:25 +0530

    Refactor installation script to remove desktop dependencies and streamline virtual environment setup

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ FLAG_3=0
 # Moving files to desktop and making virtual environment and installing all the dependencies #
 ##############################################################################################
 
-if test -f ~/Desktop/MSIFanControl/MSIFanControl.py; then
+if test -f ./bin/python; then
     FLAG_3=1
 else
     echo "This is a shell script to install all the dependencies required for this software to run."
@@ -17,15 +17,12 @@ else
     echo "2 -> PyGObject"
     echo "3 -> PyCairo"
     echo "4 -> Expert"
-    echo "----------Creating Folder for Open Freeze Center----------"
-    cd ~/Desktop
-    mkdir MSIFanControl
     echo "----------Installing python3-virtualenv AND python3-venv and other dependencies----------"
     sudo apt update
     sudo apt upgrade
     sudo apt install python3-virtualenv python3-venv libgirepository1.0-dev libcairo2-dev
-    echo "----------Creating Virtual Environment for Open Freeze Center----------"
-    python3 -m venv ~/Desktop/MSIFanControl
+    echo "----------Creating Virtual Environment----------"
+    python3 -m venv ./
     echo "----------Virtual Environment for Open Freeze Center created----------"
     echo "----------Installing PyGObject----------"
     ~/Desktop/MSIFanControl/bin/pip3 install PyGObject==3.50.0
@@ -35,13 +32,6 @@ else
     ~/Desktop/MSIFanControl/bin/pip3 install config
     echo "----------Installing Expert----------"
     sudo apt-get install expect
-    echo "----------Moving files to virtual environment----------"
-    cp -i ~/Downloads/MSIFanControl/install.sh ~/Desktop/MSIFanControl
-    cp -i ~/Downloads/MSIFanControl/file_1.sh ~/Desktop/MSIFanControl
-    cp -i ~/Downloads/MSIFanControl/file_2.sh ~/Desktop/MSIFanControl
-    cp -i ~/Downloads/MSIFanControl/MSIFanControl.py ~/Desktop/MSIFanControl
-    cp -i ~/Downloads/MSIFanControl/README.md ~/Desktop/MSIFanControl
-    cp -i ~/Downloads/MSIFanControl/LICENSE ~/Desktop/MSIFanControl
     FLAG_3=1
 fi
 
@@ -55,14 +45,12 @@ if test -d /etc/modprobe.d; then
             FLAG_1=1
         else
             echo "----------Prepairing system for EC read/write----------"
-            cd ~/Desktop/MSIFanControl/
             sudo ./file_1.sh
             FLAG_1=1
         fi
     else
         echo "----------Prepairing system for EC read/write----------"
         sudo touch /etc/modprobe.d/ec_sys.conf
-        cd ~/Desktop/MSIFanControl/
         sudo ./file_1.sh
         FLAG_1=1
     fi
@@ -70,7 +58,6 @@ else
     echo "----------Prepairing system for EC read/write----------"
     mkdir /etc/modprobe.d
     sudo touch /etc/modprobe.d/ec_sys.conf
-    cd ~/Desktop/MSIFanControl/
     sudo ./file_1.sh
     FLAG_1=1
 fi
@@ -81,14 +68,12 @@ if test -d /etc/modules-load.d; then
             FLAG_2=1
         else
             echo "----------Prepairing system for EC read/write----------"
-            cd ~/Desktop/MSIFanControl/
             sudo ./file_2.sh
             FLAG_2=1
         fi
     else
         echo "----------Prepairing system for EC read/write----------"
         sudo touch /etc/modules-load.d/ec_sys.conf
-        cd ~/Desktop/MSIFanControl/
         sudo ./file_2.sh
         FLAG_2=1
     fi
@@ -96,7 +81,6 @@ else
     echo "----------Prepairing system for EC read/write----------"
     mkdir /etc/modules-load.d
     sudo touch /etc/modules-load.d/ec_sys.conf
-    cd ~/Desktop/MSIFanControl/
     sudo ./file_2.sh
     FLAG_2=1
 fi
@@ -108,11 +92,11 @@ else
 fi
 
 if [ "$FLAG_3" -eq 1 ]; then
-    if test -f ~/Desktop/MSIFanControl/config.py; then
+    if test -f ./config.py; then
         echo "----------Running Software----------"
-        sudo nohup ~/Desktop/MSIFanControl/bin/python3 ~/Desktop/MSIFanControl/MSIFanControl.py
+        sudo nohup ./bin/python3 ~/Desktop/MSIFanControl/MSIFanControl.py
     else
         echo "----------Running Software----------"
-        sudo nohup ~/Desktop/MSIFanControl/bin/python3 ~/Desktop/MSIFanControl/MSIFanControl.py
+        sudo nohup ./bin/python3 ~/Desktop/MSIFanControl/MSIFanControl.py
     fi
 fi
